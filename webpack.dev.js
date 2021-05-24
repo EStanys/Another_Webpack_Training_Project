c03ba452cfc7e3c8631c1f5f09e24a257db9c895
EStanys <edvinasstanys@gmail.com>
2021-05-24 12:03:39 +0300

    template parameters, title

--- webpack.dev.js
+++ webpack.dev.js
@@ -12,6 +12,13 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "/src/html/template.html", // nurodom kelia is kur pasiimt template pagal kuri kurs html faila dist foldery
+      templateParameters: {
+        title: "We now know Webpack", // ka irasysim cia atsivaizduos index html ten kur busim ideja <%= title%> template.html faile
+      },
+      minify: {
+        removeComments: true, // sugeneruotam index.html nerodys komentaru musu parasytu template.html faile
+        collapseWhitespace: false,
+      },
     }),
   ],
 };
